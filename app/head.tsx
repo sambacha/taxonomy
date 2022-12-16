8b29db2e139266f9fdc02b2c8db60b9c3a6b28f6
Sam Bacha <sam@manifoldfinance.com>
2022-12-16 00:27:45 -0800

    fix fmt

--- app/head.tsx
+++ app/head.tsx
@@ -4,19 +4,22 @@ export default function Head() {
       <title>Manifold Finance</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width" />
-      <meta
-        name="Manifold Finance"
-        content="An open source protocol for MEV"
-      />
+      <meta name="Manifold Finance" content="An open source protocol for MEV" />
 
       <meta property="og:title" content="Manifold Finance" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://manifoldfinance.com" />
-      <meta property="og:image" content="https://www.manifoldfinance.com/social.png" />
+      <meta
+        property="og:image"
+        content="https://www.manifoldfinance.com/social.png"
+      />
       <meta name="twitter:title" content="Manifold Finance" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://manifoldfinance.com" />
-      <meta name="twitter:image" content="https://www.manifoldfinance.com/social.png" />
+      <meta
+        name="twitter:image"
+        content="https://www.manifoldfinance.com/social.png"
+      />
     </>
   )
 }
